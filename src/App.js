1a49b04b2e4257bcf737b836a2db144b6560c519
Ohi20 <ashrafohi20@gmail.com>
2022-05-11 11:13:20 +0600

    navbar created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
